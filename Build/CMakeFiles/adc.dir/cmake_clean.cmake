8478a116dbc3934cebbaf06cfabcd75b0324e1aa
Andriy Tkachyshyn <Andriy.Tkachyshyn@thomascook.se>
2018-06-29 14:40:25 +0300

    removed build folder from source control

---
+++ Build/CMakeFiles/adc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/adc.dir/adc.c.o"
+  "libadc.pdb"
+  "libadc.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/adc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
